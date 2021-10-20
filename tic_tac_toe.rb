bb9855e5c7d028bd4c37b5f2b2341c5ee46d03b6
crespire <li.simmon@gmail.com>
2021-10-20 13:17:58 -0400

    Complete initial show_board implementation

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,21 +1,22 @@
 class TicTacToe
-    attr_reader :turn
+  attr_reader :turn
 
-    def initialize(size=3)
-        @turn = 0
-        @size = size
-        @board = Array.new(size) {Array.new(size, nil)}
+  def initialize(size=3)
+    @turn = 0
+    @size = size
+    @board = Array.new(size) {Array.new(size, nil)}
+  end
+
+  def show_board
+    @board.each do |row|
+      row.each do |value|
+        to_display = value.nil? ? "  " : value.to_s + " "
+        print to_display
+      end
+      puts "\n"
     end
-
-    def show_board
-        @board.each do |row|
-            row.each do |value|
-                print value.to_s + " "
-            end
-            puts "\n"
-        end
-    end
-
+    puts "\n"
+  end
 end
 
 game = TicTacToe.new()
